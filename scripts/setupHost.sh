674cb2af65d6d78e43fff67e3c60106ac1490a4b
Josh Priestley <josh.priestley@bbc.co.uk>
2014-06-05 11:48:18 +0100

    Removing uneeded variable escaping

--- scripts/setupHost.sh
+++ scripts/setupHost.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-sudo ln -s {$PWD}/host-serf-conf /etc/serf
+sudo ln -s $PWD/host-serf-conf /etc/serf
 sudo ln -s /etc/serf/serf.init.sh /etc/init.d/serf
-sudo ln -s {$PWD}/tviplayer-node/serf-0.5.0 /usr/bin/serf
+sudo ln -s $PWD/tviplayer-node/serf-0.5.0 /usr/bin/serf
 
 sudo /etc/init.d/serf start
 
