f349fe9ea13f69580b52740c8dc9a04907a37dc6
Josh Priestley <josh.priestley@bbc.co.uk>
2014-06-05 11:55:47 +0100

    Changing docker install method for 14

--- scripts/installDocker.sh
+++ scripts/installDocker.sh
@@ -3,16 +3,12 @@
 set -e
 
 sudo apt-get update
-sudo apt-get -y install linux-image-extra-name -r
-mkdir -p /var/lib/docker
-mount -M /mnt /var/lib/docker || true
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+# Uncomment the following to have docker installed into /mnt instead (more disk space?)
+#mkdir -p /var/lib/docker
+#mount -M /mnt /var/lib/docker || true
 
-sudo sh -c "echo deb http://get.docker.io/ubuntu docker main\
-> /etc/apt/sources.list.d/docker.list"
 sudo apt-get update
-sudo apt-get -y install lxc-docker
+sudo apt-get -y install docker.io
 
-sudo apt-get update && sudo apt-get -y install cgroup-lite
 
